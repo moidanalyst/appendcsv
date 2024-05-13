2a06ee76bf0944ed91a5c2289fbdfd2be1c8e1cf
Moid Ahmed Siddiqui <156670272+moidanalyst@users.noreply.github.com>
2024-05-13 16:22:18 +0500

    append.py

--- append.py
+++ append.py
@@ -1,18 +1,10 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[7]:
-
 import os
 import pandas as pd
 
-# In[8]:
-
-folder_path = r"D:\OneDrive_Data\OneDrive - The Citizens Foundation\Desktop\February 2024\Subsidy\csv"
+folder_path = r"D:\OneDrive_Data\OneDrive\csv" # Your File Path Here
 fileslist = os.listdir(folder_path)
 
-# In[9]:
-
+# Create an empty list and append all data frames into it
 dfs = []
 
 for index, files in enumerate(fileslist):
@@ -21,28 +13,6 @@ for index, files in enumerate(fileslist):
     df.rename(columns={'Unnamed: 0': ''}, inplace=True)
     dfs.append(df)
 
+# Combine all dataframes in the list in one dataframe and generate excel file
 combined_df = pd.concat(dfs, ignore_index=True)
-
-# In[10]:
-
-combined_df[combined_df["Account Description"] == "B1. Sub Total"]
-
-# In[11]:
-
-combined_df
-
-# In[12]:
-
-combined_df = combined_df[combined_df["Account Description"] != "B1. Sub Total"]
-
-# In[13]:
-
-combined_df
-
-# In[14]:
-
-combined_df.to_excel("subsidydata.xlsx")
-
-
-
-
+combined_df.to_excel("data.xlsx")
